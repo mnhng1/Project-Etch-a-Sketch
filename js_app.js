5ed5a34ff5b52b47db8bb14feb85e71aedf04e0c
Minh T Nguyen <yourname@example.com>
2024-04-18 01:00:38 -0400

    Finishing debugging. Fixing the reset and add a click to set size function to set the amounts of grids in the same container size. Challenge is skip

--- js_app.js
+++ js_app.js
@@ -2,37 +2,52 @@ const container = document.querySelector(".container");
 
 
 
-
-for (let i = 1; i <=256; i++){
-    const grid = document.createElement('div');
-    grid.className = "grids"
-    grid.style.border = "1px solid black";
-    grid.style.backgroundColor = "white";
-    grid.style.height = "6.25%";
-    grid.style.width = "6.25%";
-    grid.style.boxSizing = "border-box";
-    grid.addEventListener("mouseover", event=>{event.target.style.backgroundColor = "purple";}) //event is an automatically object create when an event (such as hover) occur. .target is then use to acceses the element (div) that trigger such event. 
+function setGrid(size=16){
+    const gridSize = 100/size;
     
-    container.appendChild(grid);
-    
+    for (let i = 1; i <=(size*size); i++){
+        const grid = document.createElement('div');
+        grid.className = "grids"
+        grid.style.border = "1px solid black";
+        grid.style.backgroundColor = "white";
+        grid.style.height = gridSize+"%";
+        grid.style.width = gridSize+"%";
+        grid.style.boxSizing = "border-box";
+        grid.addEventListener("mouseover", event=>{event.target.style.backgroundColor = "purple";}) //event is an automatically object create when an event (such as hover) occur. .target is then use to acceses the element (div) that trigger such event. 
+        
+        container.appendChild(grid);
+        
+    }
 }
 
-const allgrid = document.querySelectorAll(".grids");
+
+
+
+
 
 const resetButton = document.querySelector(".reset");
 resetButton.addEventListener("click", () => {
-    console.log('Hello')
-    allgrid.forEach(grid => {
-        grid.style.backgroundColor = "white";
-    });
-});
-
-
-allgrid.forEach(grid => {
-    grid.style.flexGrow = "1";
+    const allgrid = document.querySelectorAll(".grids");
+    allgrid.forEach(grid => {grid.remove()});
+    setGrid();
     
 });
 
 
 
+
+const setSize = document.querySelector("#setSize");
+setSize.addEventListener("click", () => { 
+    const allgrid = document.querySelectorAll(".grids");
+    let size = prompt("Enter Dimension (Num of box per row/column)"); 
+    allgrid.forEach(grid => {grid.remove()});
+    setGrid(size);
+})
+    
+
+
+
+
+
+setGrid()
 
